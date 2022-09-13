1a6ba8d8a86ee569be82b232f21ebf32dfe2502d
Szarka Álmos <cfgi1o@uni-corvinus.hu>
2022-09-13 10:34:04 +0200

    masodik commit

--- masodik.py
+++ masodik.py
@@ -7,4 +7,6 @@ untracked files --> itt van pl a /main.py
 staging area --> Ha zöld a neve, akkor szeretnénk feltölteni (git add)
 
 commited area (git commit) 
+
+branchek létrehozása git branch -b "name"
 '''
