e7925aae65a07c3620ebebbb35a2d2d91089e3f7
Szarka Álmos <cfgi1o@uni-corvinus.hu>
2022-09-13 10:20:52 +0200

    elso commit

--- masodik.py
+++ masodik.py
@@ -4,7 +4,7 @@ print("git")
 '''
 untracked files --> itt van pl a /main.py 
 
-staging area --> Ha zöld a neve, akkor szeretnénk feltölteni
+staging area --> Ha zöld a neve, akkor szeretnénk feltölteni (git add)
 
-
+commited area (git commit) 
 '''
