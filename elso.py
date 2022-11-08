8a2704ba2a953d2916cd58f79ef2e5b74f290347
almos0121 <almos0121@gmail.com>
2022-11-08 11:11:10 +0100

    gyak vege

--- elso.py
+++ elso.py
@@ -3,6 +3,7 @@ from scipy.stats import norm
 from matplotlib import pyplot as plt
 
 from hatodikgyak import Option
+from hatodikgyak import GBrownianPath
 K = 360
 
 expiry = "20221215"
@@ -22,11 +23,55 @@ spots = range(250,500,5)
 prices = [C.calcPrice(s,1, vola, r) for s in spots]
 pays = [C.calcPayoff(s) for s in spots]
 
-plt.plot(spots,pays, spots, prices)
-plt.show()
+# plt.plot(spots,pays, spots, prices)
+# plt.show()
 
 # print(opt.calcPayoff(139))
 # print(opt.calcPrice(110,0.5,0.4,0.3))
 
 
 #option profit calculator
+
+#creating options
+opt_c = Option("C",380, None, 1)
+opt_p = Option("P",380, None, 1)
+print(opt_p)
+
+opt_c.vola = 0.3
+opt_p.vola = 0.3
+
+a = opt_c.calcPrice(362,1/12, opt_c.vola, 0.1)
+# opt_p.calcPrice(362,1/12)
+b = opt_c.calcVola(a,362,1/12,0.1) #nem jó volat ad vissza, mi a baj? A vola_hi-t kellett alakítgatni
+print(a)
+print(b)
+
+#Generating Brownian Path
+
+gb = GBrownianPath()
+
+sigma = 0.35
+N=250
+S0=100
+spots1 = gb.generate(S0,0.,sigma,1,N)
+times = np.arange(0,1,1/N)
+plt.plot(times,spots1)
+# plt.show()
+
+opt = Option("C", S0, None, 1)
+
+vola = 0.3
+prices = []
+deltas = []
+
+#kiszámlom minden egyes pontban az opció árát
+
+for (t,S) in zip(times, spots1): #Ez a zip párokon iterál végig (t,S) párok az ábrázoláshoz
+    price = opt.calcPrice(S,1-t, vola,0)
+    delta = opt.calcDelta(S, 1-t, vola, 0)
+    prices.append(price)
+    deltas.append(delta)
+
+
+plt.plot(times,np.array(prices))
+plt.show()
--- hatodikgyak.py
+++ hatodikgyak.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import norm
-
+#ár a vola-nak mon növő függvénye
+#Most árhoz kell megmondani a volatilitást --> invertálni kell kb a CalcPrice functiont --> Iteratív módon kellene, beletszünk valamit és kisebb vagy nagyobb lesz
 
 class Option:
 
@@ -43,3 +44,49 @@ class Option:
         else:
             print("Wrong option type")
             return None
+    def calcVola(self, price : float,S: float, time: float, rate:float ):
+        #kezdőérték fontos, szigmákkal körül kellene lőni az árat
+        vola_hi = 0.5
+        while self.calcPrice(S, vola_hi, time, rate) < price:
+            vola_hi *= 2
+        vola_low = vola_hi / 2
+        while abs(vola_hi - vola_low) > 0.0001:
+            vola = 0.5 * (vola_low + vola_hi)
+            price_update = self.calcPrice(S, time, vola, rate)
+            if price_update < price:
+                vola_low = vola
+            else:
+                vola_hi = vola
+        return vola #ez kvázi a vola átlag
+
+    def calcDelta(self, S, timeToExp, vola, rate=0):
+        if not np.isnan(vola):
+            IV = vola
+        else:
+            IV = self.vola if not np.isnan(self.vola) else self.initVola
+        if np.isnan(IV):
+            print("Vola is not set!")
+            return np.nan
+        t = timeToExp
+        if t > 0:
+            d1 = (np.log(S / self.strike) + (rate + IV ** 2 / 2) * t) / (IV * np.sqrt(t))
+            if self.right == 'C':
+                return norm.cdf(d1) * self.pos
+            else:
+                return (norm.cdf(d1) - 1) * self.pos
+        else:
+            print("expired!")
+            return np.nan
+
+
+class GBrownianPath:
+
+    def __init__(self):
+        pass
+
+    def generate (self,S0,mu,sigma,T,N):
+        #dS 7 mu*S*dt + sigma*S*dWt
+        dt = T/N
+        X = np.exp((mu-sigma**2/2)*dt+sigma*np.random.normal(0,np.sqrt(dt),N))
+        return S0 * np.cumprod(X)
+
